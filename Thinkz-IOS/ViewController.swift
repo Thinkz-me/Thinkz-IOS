0a97fa64628758dc2bfc0f9cbc8584ab2d8ede62
Mateusz Łukasiński <mat.lukasinski@gmail.com>
2020-03-22 01:34:32 +0100

    Initial Commit

---
+++ Thinkz-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Thinkz-IOS
+//
+//  Created by Mateusz Łukasiński on 22/03/2020.
+//  Copyright © 2020 Thinkz.me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
